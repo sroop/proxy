de8c496ebd84addb44ff846693b697e41b2a9ec6
Sroop Sunar <sroop@users.noreply.github.com>
2014-08-01 13:05:03 +0100

    need to fix tests

--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -10,32 +10,22 @@ describe 'Proxy' do
     Sinatra::Application
   end
 
-  context 'The proxy forwards to the server correctly' do
-  
-    it 'the proxy forwards client requests to the server' do
-      get('http://localhost:9000/')
-      stubbed_get = stub_request(:get, 'http://localhost:4567/')
-      expect(stubbed_get).to have_been_requested
-    end
-
-    it 'client requests to the proxy correctly returns the right response from the server' do
-      get('http://localhost:9000/hello_world')
-      expect(last_response.body).to eq("hello world")
-    end
-
+  it 'The proxy does not forward to the server without the API key' do
+    expect{get('http://localhost:9000/')}.to raise_error("No API key")
   end
 
   context 'The server only responds to client requests via the proxy' do
 
-    it 'status code 400 when client requests directly to the server' do
-      get('http://localhost:4567/')
-      expect(last_response.status).to eq(400)
-    end
+    # it 'status code 400 when client requests directly to the server' do
+    #   get 'localhost:9000', headers: { 'X-API-KEY' => 'test:test' }
+    #   stubbed_get = stub_request(:get, "http://localhost:4567")
+    #   expect(stubbed_get).to have_been_requested
+    # end
 
-    it 'status code 200 when the client requests via the the proxy' do
-      get('http://localhost:9000/')
-      expect(last_response.status).to eq(200)
-    end
+    # it 'status code 200 when the client requests via the the proxy' do
+    #   get('http://localhost:9000/')
+    #   expect(last_response.status).to eq(200)
+    # end
 
   end
 
